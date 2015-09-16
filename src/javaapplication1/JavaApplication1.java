d2856017e725ff63d7f1bb4c6fd0d83f77397a1c
mihir <mihir@192.168.2.27>
2015-09-16 00:29:51 -0400

    Initial

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author mihir
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /***********************
+     * Assignment 1
+     * Job Posting
+     * http://autodata.applytojob.com/apply/2d17200c5f5b757277504063695b5e7848621537151c3b451e2f29210d372e081e3112/Intermediate-Java-Developer?source=INDE&sid=GxBSmkDGGgrZXnlAzWJtEHasWvmd5dzLKbD&source=INDE
+     * INTERMEDIATE JAVA DEVELOPER
+       LOCATION : LONDON, ONTARIO
+       REQUIRED EXPERIENCE : MID-LEVEL (MINIMUM)
+
+     
+     * J2EE  https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+     * HTML 5  https://en.wikipedia.org/wiki/HTML5
+     * JQUERY  https://en.wikipedia.org/wiki/JQuery
+     * SPRING FRAMEWORK  https://en.wikipedia.org/wiki/Spring_Framework
+     * MYBATIS  https://en.wikipedia.org/wiki/MyBatis
+     * C++  https://en.wikipedia.org/wiki/C%2B%2B
+     * C#  https://en.wikipedia.org/wiki/C_Sharp_%28programming_language%29
+     * TOMCAT  https://en.wikipedia.org/wiki/Apache_Tomcat
+     * MAVEN  https://en.wikipedia.org/wiki/Maven
+     * SQL  https://en.wikipedia.org/wiki/SQL
+     
+     
+     * Toronto Salary $475k
+     * Vancouver Salary $86k
+     * Waterloo Salary $75k
+     * 
+     * 
+    ***********************/
+    }
+    
+}
